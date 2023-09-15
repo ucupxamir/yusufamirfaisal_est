5a1b58799551430e7c5c0e24683456732506000a
ucupxamir <ucupxamir@users.noreply.github.com>
2023-09-15 21:16:02 +0700

    add cors

--- backend/src/application/web.js
+++ backend/src/application/web.js
@@ -1,8 +1,15 @@
 import express from "express";
-import {errorMiddleware} from "../middleware/error-middleware.js";
-import {router} from "../route/api.js";
+import cors from 'cors'
+import { errorMiddleware } from "../middleware/error-middleware.js";
+import { router } from "../route/api.js";
 
 export const web = express();
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'GET', 'DELETE']
+}
+
+web.use(cors(corsOptions));
 web.use(express.json());
 web.use(router);
 
